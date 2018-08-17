cb75282c0857a7a95e43cf163ed25706bce7497f
RoniAngress <roniangress32@gmail.com>
2018-08-17 13:08:30 +0300

    Initial commit

---
+++ app.java
@@ -0,0 +1,10 @@
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My App!");
+	}
+
+}
